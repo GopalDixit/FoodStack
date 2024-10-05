1f968ada1f6794bc53d3092e1196dd7ad59e1ff1
Gopal Dixit <98686075+GopalDixit@users.noreply.github.com>
2024-10-05 14:44:22 +0530

    Update Navbar.jsx

--- FoodStack/src/components/Navbar.jsx
+++ FoodStack/src/components/Navbar.jsx
@@ -4,24 +4,36 @@ import { AuthContext } from './AuthContext';
 
 const Navbar = () => {
   const { isAuthenticated, logout, userRole } = useContext(AuthContext);
-
+  
   return (
-    <nav className="bg-orange-800 text-white py-4">
+    <nav className={`text-white py-4 fixed top-0 left-0 w-full z-10 ${isAuthenticated ? 'bg-orange-800' : 'bg-transparent'}`}>
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-lg font-bold font-extralight ">Food <span className="bg-green-400 p-2 text-white rounded-xl hover:font-bold">Stack</span></Link>
+        <Link to="/" className="text-lg font-bold font-extralight">Food <span className="bg-green-400 p-2 text-white rounded-xl hover:font-bold">Stack</span></Link>
         <div>
           {isAuthenticated ? (
             <>
               {userRole === 'admin' && <Link to="/add-food" className="mr-4">Add Food</Link>}
               <Link to="/" className="mr-4">Home</Link>
-              {/* <Link to="/cartitem" className="mr-4">Cart</Link> */}
-
               <button onClick={logout} className="bg-green-500 px-4 py-2 rounded">Logout</button>
             </>
           ) : (
             <>
-              <Link to="/login" className="mr-4">Login</Link>
-              <Link to="/signup" className="bg-green-500 px-4 py-2 rounded-lg">Signup</Link>
+              <Link 
+                to="/login" 
+                className="mr-4 text-lg font-serif" 
+                style={{zIndex:12}}
+                onClick={() => console.log("Navigating to Login")}
+              >
+                Login
+              </Link>
+              <Link 
+                to="/signup" 
+                className="bg-green-500 px-4 py-2 rounded-lg" 
+                style={{zIndex:12}}
+                onClick={() => console.log("Navigating to Signup")}
+              >
+                Signup
+              </Link>
             </>
           )}
         </div>
@@ -29,5 +41,7 @@ const Navbar = () => {
     </nav>
   );
 };
+
+
 
 export default Navbar;
