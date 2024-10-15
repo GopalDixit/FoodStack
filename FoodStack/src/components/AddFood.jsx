d05196740cdadb517771387f1b72d34017dad256
Gopal Dixit <98686075+GopalDixit@users.noreply.github.com>
2024-10-15 07:58:36 +0530

    Update AddFood.jsx

--- FoodStack/src/components/AddFood.jsx
+++ FoodStack/src/components/AddFood.jsx
@@ -17,7 +17,7 @@ const AddFood = () => {
     e.preventDefault();
     
     try {
-        const response = await fetch('http://localhost:5000/api/food', { 
+        const response = await fetch('https://foodstack-xp5k.onrender.com/api/food', { 
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
